3abc08b9d434badb98a4e93a5fa0184a5618f0f4
Betul Avcil <blavcil@gmail.com>
2024-10-06 18:58:13 +0300

    update

--- Telco_Customer_Churn_Solution.py
+++ Telco_Customer_Churn_Solution.py
@@ -1,34 +1,42 @@
-# Diabetes Feature Engineering
-
-
+# Telco Customer Churn Feature Engineering
 
 # Gerekli Kütüphane ve Fonksiyonlar
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from catboost import CatBoostClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
-from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import warnings
 warnings.simplefilter(action="ignore")
 
 pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_rows', 20)
+pd.set_option('display.width', 170)
+pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
-
-df = pd.read_csv("diabetes.csv")
+df = pd.read_csv("Telco-Customer-Churn.csv")
 df.head()
+df.shape
+df.info()
+
+
+# TotalCharges sayısal bir değişken olmalı
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors='coerce')
+
+df["Churn"] = df["Churn"].apply(lambda x : 1 if x == "Yes" else 0)
+
+
 
 
 # GÖREV 1: KEŞİFCİ VERİ ANALİZİ
 
 # GENEL RESİM
+
 
 def check_df(dataframe, head=5):
     print("##################### Shape #####################")
@@ -46,11 +54,11 @@ def check_df(dataframe, head=5):
 
 check_df(df)
 
-df.head()
-df.info()
+
 
 
 # NUMERİK VE KATEGORİK DEĞİŞKENLERİN YAKALANMASI
+
 
 def grab_col_names(dataframe, cat_th=10, car_th=20):
     """
@@ -118,6 +126,8 @@ cat_but_car
 
 
 
+
+
 # KATEGORİK DEĞİŞKENLERİN ANALİZİ
 
 def cat_summary(dataframe, col_name, plot=False):
@@ -128,7 +138,10 @@ def cat_summary(dataframe, col_name, plot=False):
         sns.countplot(x=dataframe[col_name], data=dataframe)
         plt.show()
 
-cat_summary(df, "Outcome")
+for col in cat_cols:
+    cat_summary(df, col)
+
+
 
 
 
@@ -150,63 +163,46 @@ for col in num_cols:
 
 
 
+
 # NUMERİK DEĞİŞKENLERİN TARGET GÖRE ANALİZİ
 
 def target_summary_with_num(dataframe, target, numerical_col):
     print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
 
 for col in num_cols:
-    target_summary_with_num(df, "Outcome", col)
+    target_summary_with_num(df, "Churn", col)
+
+
+
+
+
+# KATEGORİK DEĞİŞKENLERİN TARGET GÖRE ANALİZİ
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(categorical_col)
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean(),
+                        "Count": dataframe[categorical_col].value_counts(),
+                        "Ratio": 100 * dataframe[categorical_col].value_counts() / len(dataframe)}), end="\n\n\n")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Churn", col)
+
 
 
 
 # KORELASYON
 
-# Korelasyon, olasılık kuramı ve istatistikte iki rassal değişken arasındaki doğrusal ilişkinin yönünü ve gücünü belirtir
-
-df.corr()
+df[num_cols].corr()
 
 # Korelasyon Matrisi
 f, ax = plt.subplots(figsize=[18, 13])
-sns.heatmap(df.corr(), annot=True, fmt=".2f", ax=ax, cmap="magma")
+sns.heatmap(df[num_cols].corr(), annot=True, fmt=".2f", ax=ax, cmap="magma")
 ax.set_title("Correlation Matrix", fontsize=20)
 plt.show()
 
+# TotalChargers'in aylık ücretler ve tenure ile yüksek korelasyonlu olduğu görülmekte
 
-
-
-
-# BASE MODEL KURULUMU
-
-y = df["Outcome"]
-X = df.drop("Outcome", axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
-
-rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
-y_pred = rf_model.predict(X_test)
-
-
-print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 2)}")
-print(f"Recall: {round(recall_score(y_pred,y_test),3)}")
-print(f"Precision: {round(precision_score(y_pred,y_test), 2)}")
-print(f"F1: {round(f1_score(y_pred,y_test), 2)}")
-print(f"Auc: {round(roc_auc_score(y_pred,y_test), 2)}")
-
-
-
-def plot_importance(model, features, num=len(X), save=False):
-    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
-    plt.figure(figsize=(10, 10))
-    sns.set(font_scale=1)
-    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
-                                                                     ascending=False)[0:num])
-    plt.title('Features')
-    plt.tight_layout()
-    plt.show()
-    if save:
-        plt.savefig('importances.png')
-
-plot_importance(rf_model, X)
+df.corrwith(df["Churn"]).sort_values(ascending=False)
 
 
 
@@ -216,21 +212,7 @@ plot_importance(rf_model, X)
 
 # EKSİK DEĞER ANALİZİ
 
-# Bir insanda Pregnancies ve Outcome dışındaki değişken değerleri 0 olamayacağı bilinmektedir.
-# Bundan dolayı bu değerlerle ilgili aksiyon kararı alınmalıdır. 0 olan değerlere NaN atanabilir .
 
-zero_columns = [col for col in df.columns if (df[col].min() == 0 and col not in ["Pregnancies", "Outcome"])]
-
-zero_columns
-
-
-# Gözlem birimlerinde 0 olan degiskenlerin her birisine gidip 0 iceren gozlem degerlerini NaN ile değiştirdik.
-for col in zero_columns:
-    df[col] = np.where(df[col] == 0, np.nan, df[col])
-
-
-
-# Eksik Gözlem Analizi
 df.isnull().sum()
 
 def missing_values_table(dataframe, na_name=False):
@@ -244,30 +226,40 @@ def missing_values_table(dataframe, na_name=False):
 
 na_columns = missing_values_table(df, na_name=True)
 
-
-
-# Eksik Değerlerin Bağımlı Değişken ile İlişkisinin İncelenmesi
-def missing_vs_target(dataframe, target, na_columns):
-    temp_df = dataframe.copy()
-    for col in na_columns:
-        temp_df[col + '_NA_FLAG'] = np.where(temp_df[col].isnull(), 1, 0)
-    na_flags = temp_df.loc[:, temp_df.columns.str.contains("_NA_")].columns
-    for col in na_flags:
-        print(pd.DataFrame({"TARGET_MEAN": temp_df.groupby(col)[target].mean(),
-                            "Count": temp_df.groupby(col)[target].count()}), end="\n\n\n")
-
-
-missing_vs_target(df, "Outcome", na_columns)
-
-
-
-
-# Eksik Değerlerin Doldurulması
-for col in zero_columns:
-    df.loc[df[col].isnull(), col] = df[col].median()
-
+df["TotalCharges"].fillna(df["TotalCharges"].median(), inplace=True)  # aylık ödenecek miktarlarıyla totalcharge doldurulailir (daya iyi olur denensin)  veya 11 değişken drop edilebilir
 
 df.isnull().sum()
+
+
+
+
+
+# BASE MODEL KURULUMU
+
+dff = df.copy()
+cat_cols = [col for col in cat_cols if col not in ["Churn"]]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+dff = one_hot_encoder(dff, cat_cols, drop_first=True)
+
+
+y = dff["Churn"]
+X = dff.drop(["Churn","customerID"], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+catboost_model = CatBoostClassifier(verbose=False, random_state=12345).fit(X_train, y_train)
+y_pred = catboost_model.predict(X_test)
+
+
+print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 4)}")
+print(f"Recall: {round(recall_score(y_pred,y_test),4)}")
+print(f"Precision: {round(precision_score(y_pred,y_test), 4)}")
+print(f"F1: {round(f1_score(y_pred,y_test), 4)}")
+print(f"Auc: {round(roc_auc_score(y_pred,y_test), 4)}")
 
 
 
@@ -283,14 +275,12 @@ def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
     return low_limit, up_limit
 
 
-
 def check_outlier(dataframe, col_name):
     low_limit, up_limit = outlier_thresholds(dataframe, col_name)
     if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
         return True
     else:
         return False
-
 
 
 def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
@@ -301,75 +291,69 @@ def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
 
 
 # Aykırı Değer Analizi ve Baskılama İşlemi
-for col in df.columns:
+for col in num_cols:
     print(col, check_outlier(df, col))
     if check_outlier(df, col):
         replace_with_thresholds(df, col)
 
-for col in df.columns:
-    print(col, check_outlier(df, col))
 
 
 
 
 # ÖZELLİK ÇIKARIMI
 
-# Yaş değişkenini kategorilere ayırıp yeni yaş değişkeni oluşturulması
-df.loc[(df["Age"] >= 21) & (df["Age"] < 50), "NEW_AGE_CAT"] = "mature"
-df.loc[(df["Age"] >= 50), "NEW_AGE_CAT"] = "senior"
-
-
-# BMI 18,5 aşağısı underweight, 18.5 ile 24.9 arası normal, 24.9 ile 29.9 arası Overweight ve 30 üstü obez
-df['NEW_BMI'] = pd.cut(x=df['BMI'], bins=[0, 18.5, 24.9, 29.9, 100],labels=["Underweight", "Healthy", "Overweight", "Obese"])
-
-
-# Glukoz degerini kategorik değişkene çevirme
-df["NEW_GLUCOSE"] = pd.cut(x=df["Glucose"], bins=[0, 140, 200, 300], labels=["Normal", "Prediabetes", "Diabetes"])
-
-
-# # Yaş ve beden kitle indeksini bir arada düşünerek kategorik değişken oluşturma 3 kırılım yakalandı
-df.loc[(df["BMI"] < 18.5) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "underweightmature"
-df.loc[(df["BMI"] < 18.5) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "underweightsenior"
-df.loc[((df["BMI"] >= 18.5) & (df["BMI"] < 25)) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "healthymature"
-df.loc[((df["BMI"] >= 18.5) & (df["BMI"] < 25)) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "healthysenior"
-df.loc[((df["BMI"] >= 25) & (df["BMI"] < 30)) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "overweightmature"
-df.loc[((df["BMI"] >= 25) & (df["BMI"] < 30)) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "overweightsenior"
-df.loc[(df["BMI"] > 18.5) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_BMI_NOM"] = "obesemature"
-df.loc[(df["BMI"] > 18.5) & (df["Age"] >= 50), "NEW_AGE_BMI_NOM"] = "obesesenior"
-
-
-# Yaş ve Glikoz değerlerini bir arada düşünerek kategorik değişken oluşturma
-df.loc[(df["Glucose"] < 70) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "lowmature"
-df.loc[(df["Glucose"] < 70) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "lowsenior"
-df.loc[((df["Glucose"] >= 70) & (df["Glucose"] < 100)) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "normalmature"
-df.loc[((df["Glucose"] >= 70) & (df["Glucose"] < 100)) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "normalsenior"
-df.loc[((df["Glucose"] >= 100) & (df["Glucose"] <= 125)) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "hiddenmature"
-df.loc[((df["Glucose"] >= 100) & (df["Glucose"] <= 125)) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "hiddensenior"
-df.loc[(df["Glucose"] > 125) & ((df["Age"] >= 21) & (df["Age"] < 50)), "NEW_AGE_GLUCOSE_NOM"] = "highmature"
-df.loc[(df["Glucose"] > 125) & (df["Age"] >= 50), "NEW_AGE_GLUCOSE_NOM"] = "highsenior"
+# Tenure  değişkeninden yıllık kategorik değişken oluşturma
+df.loc[(df["tenure"]>=0) & (df["tenure"]<=12),"NEW_TENURE_YEAR"] = "0-1 Year"
+df.loc[(df["tenure"]>12) & (df["tenure"]<=24),"NEW_TENURE_YEAR"] = "1-2 Year"
+df.loc[(df["tenure"]>24) & (df["tenure"]<=36),"NEW_TENURE_YEAR"] = "2-3 Year"
+df.loc[(df["tenure"]>36) & (df["tenure"]<=48),"NEW_TENURE_YEAR"] = "3-4 Year"
+df.loc[(df["tenure"]>48) & (df["tenure"]<=60),"NEW_TENURE_YEAR"] = "4-5 Year"
+df.loc[(df["tenure"]>60) & (df["tenure"]<=72),"NEW_TENURE_YEAR"] = "5-6 Year"
 
 
 
-# İnsulin Değeri ile Kategorik değişken türetmek
-def set_insulin(dataframe, col_name="Insulin"):
-    if 16 <= dataframe[col_name] <= 166:
-        return "Normal"
-    else:
-        return "Abnormal"
-
-df["NEW_INSULIN_SCORE"] = df.apply(set_insulin, axis=1)
-
-df["NEW_GLUCOSE*INSULIN"] = df["Glucose"] * df["Insulin"]
-
-# sıfır olan değerler dikkat!!!!
-df["NEW_GLUCOSE*PREGNANCIES"] = df["Glucose"] * df["Pregnancies"]
-#df["NEW_GLUCOSE*PREGNANCIES"] = df["Glucose"] * (1+ df["Pregnancies"])
+# Kontratı 1 veya 2 yıllık müşterileri Engaged olarak belirtme
+df["NEW_Engaged"] = df["Contract"].apply(lambda x: 1 if x in ["One year","Two year"] else 0)
 
 
-# Kolonların büyültülmesi
-df.columns = [col.upper() for col in df.columns]
+# Herhangi bir destek, yedek veya koruma almayan kişiler
+df["NEW_noProt"] = df.apply(lambda x: 1 if (x["OnlineBackup"] != "Yes") or (x["DeviceProtection"] != "Yes") or (x["TechSupport"] != "Yes") else 0, axis=1)
+
+
+# Aylık sözleşmesi bulunan ve genç olan müşteriler
+df["NEW_Young_Not_Engaged"] = df.apply(lambda x: 1 if (x["NEW_Engaged"] == 0) and (x["SeniorCitizen"] == 0) else 0, axis=1)
+
+
+
+# Kişinin toplam aldığı servis sayısı
+df['NEW_TotalServices'] = (df[['PhoneService', 'InternetService', 'OnlineSecurity',
+                                       'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                       'StreamingTV', 'StreamingMovies']]== 'Yes').sum(axis=1)
+
+
+
+# Herhangi bir streaming hizmeti alan kişiler
+df["NEW_FLAG_ANY_STREAMING"] = df.apply(lambda x: 1 if (x["StreamingTV"] == "Yes") or (x["StreamingMovies"] == "Yes") else 0, axis=1)
+
+
+# Kişi otomatik ödeme yapıyor mu?
+df["NEW_FLAG_AutoPayment"] = df["PaymentMethod"].apply(lambda x: 1 if x in ["Bank transfer (automatic)","Credit card (automatic)"] else 0)
+
+
+# ortalama aylık ödeme
+df["NEW_AVG_Charges"] = df["TotalCharges"] / (df["tenure"] + 1)
+
+
+# Güncel Fiyatın ortalama fiyata göre artışı
+df["NEW_Increase"] = df["NEW_AVG_Charges"] / df["MonthlyCharges"]
+
+
+# Servis başına ücret
+df["NEW_AVG_Service_Fee"] = df["MonthlyCharges"] / (df['NEW_TotalServices'] + 1)
+
 
 df.head()
+df.shape
 
 
 
@@ -397,7 +381,7 @@ for col in binary_cols:
 
 # One-Hot Encoding İşlemi
 # cat_cols listesinin güncelleme işlemi
-cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["OUTCOME"]]
+cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["Churn", "NEW_TotalServices"]]
 cat_cols
 
 def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
@@ -411,56 +395,22 @@ df.head()
 
 
 
-# STANDARTLAŞTIRMA
-
-num_cols
-
-scaler = StandardScaler()
-df[num_cols] = scaler.fit_transform(df[num_cols])
-
-df.head()
-df.shape
-
-
-
 
 # MODELLEME
 
-y = df["OUTCOME"]
-X = df.drop("OUTCOME", axis=1)
+y = df["Churn"]
+X = df.drop(["Churn","customerID"], axis=1)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
 
-rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
-y_pred = rf_model.predict(X_test)
+catboost_model = CatBoostClassifier(verbose=False, random_state=12345).fit(X_train, y_train)
+y_pred = catboost_model.predict(X_test)
 
 print(f"Accuracy: {round(accuracy_score(y_pred, y_test), 2)}")
-print(f"Recall: {round(recall_score(y_pred,y_test),3)}")
+print(f"Recall: {round(recall_score(y_pred,y_test),2)}")
 print(f"Precision: {round(precision_score(y_pred,y_test), 2)}")
 print(f"F1: {round(f1_score(y_pred,y_test), 2)}")
 print(f"Auc: {round(roc_auc_score(y_pred,y_test), 2)}")
-
-
-
-
-
-
-# FEATURE IMPORTANCE
-
-def plot_importance(model, features, num=len(X), save=False):
-    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
-    print(feature_imp.sort_values("Value",ascending=False))
-    plt.figure(figsize=(10, 10))
-    sns.set(font_scale=1)
-    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
-                                                                     ascending=False)[0:num])
-    plt.title('Features')
-    plt.tight_layout()
-    plt.show()
-    if save:
-        plt.savefig('importances.png')
-
-plot_importance(rf_model, X)
-
 
 
 
